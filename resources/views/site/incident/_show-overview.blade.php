ceabbdeec57fe1457eb36cd28877bf8f3944582a
vagrant <vagrant@homestead>
2022-01-12 07:22:12 +0000

    update

--- resources/views/site/incident/_show-overview.blade.php
+++ resources/views/site/incident/_show-overview.blade.php
@@ -8,7 +8,7 @@
             <div class="actions">
                 @if ($pEdit)
                     <a class="btn btn-circle green btn-outline btn-sm" href="/site/incident/{{ $incident->id }}/report" target="_blank" data-original-title="PDF"><i class="fa fa-file-pdf-o"></i> Report </a>
-                    <a class="btn btn-circle green btn-outline btn-sm" href="/site/incident/{{ $incident->id }}/zip" data-original-title="PDF"><i class="fa fa-file-zip-o"></i> Zip </a>
+                    <a class="btn btn-circle green btn-outline btn-sm" href="/site/incident/{{ $incident->id }}/zip" data-original-title="PDF"><i class="fa fa-file-zip-o"></i> Download Zip </a>
 
                 @endif
             </div>
