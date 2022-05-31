91aa6ddaa3dae74de07afcfd8313e8e610cb1e41
vagrant <vagrant@homestead>
2022-05-31 02:23:00 +0000

    update

--- app/Http/Controllers/Api/MailgunZohoController.php
+++ app/Http/Controllers/Api/MailgunZohoController.php
@@ -250,11 +250,13 @@ class MailgunZohoController extends Controller {
                                 } elseif (!empty($zoho_data)) {
                                     $newData = '';
                                     if (in_array($field, $datefields)) {
-                                        list($d, $m, $y) = explode('/', $zoho_data);
-                                        $date_with_leading_zeros = sprintf('%02d', $d) . '/' . sprintf('%02d', $m) . '/' . str_pad($y, 4, "20", STR_PAD_LEFT);  // produces "-=-=-Alien"sprintf('%02d', $y);
-                                        //if ($site->{$field})
-                                        //    echo " &nbsp; $field: [" . $site->{$field}->format('j/n/y') . "] [$date_with_leading_zeros]<br>";
-                                        $newData = Carbon::createFromFormat('d/m/Y H:i', $date_with_leading_zeros . '00:00')->toDateTimeString();
+                                        if (preg_match('/^\d+\/d+\/d+$/', $zoho_data)) {
+                                            list($d, $m, $y) = explode('/', $zoho_data);
+                                            $date_with_leading_zeros = sprintf('%02d', $d) . '/' . sprintf('%02d', $m) . '/' . str_pad($y, 4, "20", STR_PAD_LEFT);  // produces "-=-=-Alien"sprintf('%02d', $y);
+                                            //if ($site->{$field})
+                                            //    echo " &nbsp; $field: [" . $site->{$field}->format('j/n/y') . "] [$date_with_leading_zeros]<br>";
+                                            $newData = Carbon::createFromFormat('d/m/Y H:i', $date_with_leading_zeros . '00:00')->toDateTimeString();
+                                        }
 
                                     } elseif (in_array($field, $yesno_fields)) {
                                         $newData = ($zoho_data == 'YES') ? 1 : 0;
