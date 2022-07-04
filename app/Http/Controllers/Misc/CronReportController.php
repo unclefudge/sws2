e8a2adc48ece2d554f9c12c1c01a1032ac0469a1
vagrant <vagrant@homestead>
2022-07-04 22:31:57 +0000

    update

--- app/Http/Controllers/Misc/CronReportController.php
+++ app/Http/Controllers/Misc/CronReportController.php
@@ -285,6 +285,7 @@ class CronReportController extends Controller {
 
         $cc = Company::find(3);
         $email_list = $cc->notificationsUsersEmailType('site.upcoming.compliance');
+        $email_list = ['fudge@jordan.net.au'];
         $emails = implode("; ", $email_list);
         echo "Sending email to $emails";
         $log .= "Sending email to $emails";
