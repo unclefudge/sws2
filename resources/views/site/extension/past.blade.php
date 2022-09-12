2641d7f295918c93ad7c08703387ef47ef1cbd6b
vagrant <vagrant@homestead>
2022-09-12 03:36:10 +0000

    update

--- resources/views/site/extension/past.blade.php
+++ resources/views/site/extension/past.blade.php
@@ -39,7 +39,7 @@
                             @foreach ($extensions as $extension)
                                 <tr>
                                     <td>{{ $extension->date->format('d/m/Y') }}</td>
-                                    <td><a href="{{ $extension->attachmentUrl }}" target="_blank"></a>{{ $extension->attachment }}</td>
+                                    <td><a href="{{ $extension->attachmentUrl }}" target="_blank">{{ $extension->attachment }}</a></td>
                                 </tr>
                             @endforeach
                             </tbody>
