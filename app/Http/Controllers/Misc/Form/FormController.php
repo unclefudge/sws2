5fede2dc518618dc3a519d09d6c3974ac789e53c
vagrant <vagrant@homestead>
2022-11-13 14:05:23 +0000

    update

--- app/Http/Controllers/Misc/Form/FormController.php
+++ app/Http/Controllers/Misc/Form/FormController.php
@@ -25,6 +25,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Yajra\Datatables\Datatables;
+use Intervention\Image\Facades\Image;
 use nilsenj\Toastr\Facades\Toastr;
 use Carbon\Carbon;
 
