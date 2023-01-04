e7bb437dcd61481e2f235b27259a08fdc4ad02a5
vagrant <vagrant@homestead>
2023-01-04 14:19:47 +1100

    update software

--- public/index.php
+++ public/index.php
@@ -53,8 +53,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
-phpinfo();
-dd('here');
+//phpinfo();
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
