d80cb1b993770fb80664eefdafb609b20d3fadb0
vagrant <vagrant@homestead>
2023-07-03 05:55:40 +0000

    update software

--- app/Http/Controllers/Misc/CronReportController.php
+++ app/Http/Controllers/Misc/CronReportController.php
@@ -610,13 +610,13 @@ class CronReportController extends Controller {
                 //
                 if ($found_request) {
                     $email_to = [env('EMAIL_DEV')];
-                    $email_user = '';
+                    $email_cc = '';
                     if (\App::environment('prod')) {
                         $email_to = (validEmail($super->email)) ? $super->email : '';
                         $email_cc = ['kistie@capecod.com.au', 'gary@capecod.com.au'];
                     }
-                    //if ($email_to)
-                    //Mail::to($email_to)->cc($email_cc)->send(new \App\Mail\Site\SiteMaintenanceSupervisorNoActionSubReport($body));
+                    if ($email_to)
+                        Mail::to($email_to)->cc($email_cc)->send(new \App\Mail\Site\SiteMaintenanceSupervisorNoActionSubReport($body));
                 }
             }
         }
