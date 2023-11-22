8dbdcc8a17113195056c1f7bde10db0725a01992
vagrant <vagrant@homestead>
2023-11-22 04:40:08 +0000

    update software

--- resources/views/manage/report/payroll.blade.php
+++ resources/views/manage/report/payroll.blade.php
@@ -132,7 +132,11 @@
                                                 {{ ($company->activeCompanyDoc(4)) ?  $company->activeCompanyDoc(4)->expiry->format('d/m/Y') : '-'}}
                                             @endif
                                         </td>
-                                        <td>{!! ($company->status) ? 'Active' : $company->deactivated->format('d/m/Y') !!}</td>
+                                        <td> @if ($company->status)
+                                                Active
+                                            @else
+                                                {!! ($company->deactivated) ? $company->deactivated->format('d/m/Y') : '-' !!}
+                                            @endif</td>
                                     </tr>
                                 @endforeach
                                 </tbody>
@@ -164,18 +168,22 @@
 @section('page-level-plugins-head')
     <link href="/assets/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css"/>
     <link href="/assets/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet"
+          type="text/css"/>
     <link href="/assets/global/plugins/datatables/datatables.min.css" rel="stylesheet" type="text/css"/>
-    <link href="/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css" rel="stylesheet" type="text/css"/>
+    <link href="/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css" rel="stylesheet"
+          type="text/css"/>
 @stop
 
 @section('page-level-plugins')
     <script src="/assets/global/plugins/bootstrap-select/js/bootstrap-select.min.js" type="text/javascript"></script>
     <script src="/assets/global/plugins/select2/js/select2.full.min.js" type="text/javascript"></script>
-    <script src="/assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js"
+            type="text/javascript"></script>
     <script src="/assets/global/scripts/datatable.js" type="text/javascript"></script>
     <script src="/assets/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>
-    <script src="/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
+    <script src="/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js"
+            type="text/javascript"></script>
 @stop
 
 @section('page-level-scripts')
