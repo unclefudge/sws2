fbb0066ac97fcb4596e02a66f1db0ea02288fdba
Fudge Jordan <fudge@jordan.net.au>
2024-01-11 11:24:32 +1100

    update software

--- app/Http/Controllers/Api/MailgunSiteNoteController.php
+++ app/Http/Controllers/Api/MailgunSiteNoteController.php
@@ -36,7 +36,7 @@ class MailgunSiteNoteController extends Controller
 
         if ($this->debug) app('log')->debug("========= SiteNote Import ==========");
 
-        // Zoho Daily log
+        // SiteNote log
         $dir = '/filebank/log/sitenote';
         if (!is_dir(public_path($dir))) mkdir(public_path($dir), 0777, true);  // Create directory if required
         $this->logfile = public_path('filebank/log/sitenote/sitenote.txt');
@@ -49,17 +49,20 @@ class MailgunSiteNoteController extends Controller
         $dir = '/filebank/tmp/sitenote';
         if (!is_dir(public_path($dir))) mkdir(public_path($dir), 0777, true);  // Create directory if required
 
-        foreach ($files as $file) {
-            //$mailgun_file = $this->retrieveMailgunFile($file['url']);  // Get file from Mailgun storage
+        $files = collect(json_decode(request()->input('attachments'), true));
+        if ($files->count()) {
+            foreach ($files as $file) {
+                //$mailgun_file = $this->retrieveMailgunFile($file['url']);  // Get file from Mailgun storage
 
-            // Save the file
-            $saved_file = public_path($dir . '/' . substr($file['name'], 0, -4) . '.' . Carbon::now()->format('YmdHis') . '.csv');
-            $guzzleClient = new Client();
-            $response = $guzzleClient->get($file['url'], ['auth' => ['api', config('services.mailgun.secret')]]);
-            file_put_contents($saved_file, $response->getBody());
-            if ($this->debug) app('log')->debug("Saving file: $saved_file");
+                // Save the file
+                $saved_file = public_path($dir . '/' . substr($file['name'], 0, -4) . '.' . Carbon::now()->format('YmdHis') . '.csv');
+                $guzzleClient = new Client();
+                $response = $guzzleClient->get($file['url'], ['auth' => ['api', config('services.mailgun.secret')]]);
+                file_put_contents($saved_file, $response->getBody());
+                if ($this->debug) app('log')->debug("Saving file: $saved_file");
 
-            //$result = $this->parseFile($saved_file);
+                //$result = $this->parseFile($saved_file);
+            }
         }
 
         return response()->json(['status' => 'ok'], 200);
