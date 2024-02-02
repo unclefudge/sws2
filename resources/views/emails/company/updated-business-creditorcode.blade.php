4c3dd568c015ce3d860c94d0a0ea94526a32eedf
Fudge Jordan <fudge@jordan.net.au>
2024-02-02 14:44:42 +1100

    update software

--- resources/views/emails/company/updated-business-creditorcode.blade.php
+++ resources/views/emails/company/updated-business-creditorcode.blade.php
@@ -1,24 +1,8 @@
-@inject('companyTypes', 'App\Http\Utilities\CompanyTypes')
-@inject('companyEntityTypes', 'App\Http\Utilities\CompanyEntityTypes')
 @component('mail::message')
     # Company Updated Business Creditor Code
 
-    {{ $company->name }} has updated their Creditor Code.
+    {{ $company->name }} has updated their Creditor Code to: {{ $company->creditor_code }}
 
-    |        |        |
-    | ------:|--------|
-    | **Name**  | {{ $company->name  }} |
-    | **Business Entity**  | {{ ($company->business_entity) ? $companyEntityTypes::name($company->business_entity) : '-' }} |
-    | **Category**  | {{ $companyTypes::name($company->category) }} |
-    | **ABN**  | {{ $company->abn  }} |
-    | **Creditor Code**  | {{ $company->creditor_code }} |
-    | **Updated By** | {{ $company->updatedBy->name  }} |
-
-
-    @component('mail::button', ['url' => config('app.url').'/company/'.$company->id])
-        View Company
-    @endcomponent
-
-    Regards,<br>
+    Regards,
     {{ config('app.name') }}
 @endcomponent
