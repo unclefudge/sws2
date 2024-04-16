0eaed8bd8ccedc86e7b64d9c64474d3fa0d0dcb5
Fudge Jordan <fudge@jordan.net.au>
2024-04-16 12:52:04 +1000

    update software

--- resources/views/site/extension/show.blade.php
+++ resources/views/site/extension/show.blade.php
@@ -307,7 +307,7 @@
         function isNumber(evt) {
             evt = (evt) ? evt : window.event;
             var charCode = (evt.which) ? evt.which : evt.keyCode;
-            const validNumbers = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 12, 33, 34, 35, 36, 37, 38, 39, 40, 45];  // top numbers, keypad, keypad (numlock)
+            const validNumbers = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 13];  // top numbers, keypad
             console.log(charCode);
             if (validNumbers.includes(charCode))
                 //if ((charCode > 31 && charCode < 48) || charCode > 57) {
