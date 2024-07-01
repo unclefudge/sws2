890acb91eeb8416c2ce86ea86715b454437ca02e
Fudge Jordan <fudge@jordan.net.au>
2024-07-02 08:24:35 +1000

    update software

--- app/Http/Controllers/Misc/CronReportController.php
+++ app/Http/Controllers/Misc/CronReportController.php
@@ -881,7 +881,7 @@ class CronReportController extends Controller
                 elseif ($user->company->lastDateOnPlanner()) {
                     //$lastDateOnPlanner = Carbon::createFromFormat('Y-m-d', $user->company->lastDateOnPlanner()->format('Y-m-d)');
 
-                    if ($user->last_login->lt($date_3_month) && $user->last_login->lt($user->company->lastDateOnPlanner()) // User not logged in or not logged in last 3 months but company has been on planner
+                    if ($user->last_login->lt($date_3_month) && $user->last_login->lt($user->company->lastDateOnPlanner())) // User not logged in or not logged in last 3 months but company has been on planner
                         $user_list3[] = $user->id;
                 }
             }
