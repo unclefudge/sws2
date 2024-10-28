93609505e5605a00784792a2a6fd65e6c8b4c154
Fudge Jordan <fudge@jordan.net.au>
2024-10-28 12:51:13 +1100

    update software

--- resources/views/layout/mainmenu.blade.php
+++ resources/views/layout/mainmenu.blade.php
@@ -414,7 +414,7 @@
                     @endif
                 @endif {{-- End - Company status == 1 --}}
 
-                @if(Auth::user()->company->subscription)
+                @if(Auth::user()->hasAnyRole2('web-admin|mgt-general-manager'))
                     <li class="menu-dropdown classic-menu-dropdown {{ (Request::is('dashboard') ? 'active' : '') }}">
                         <a href="/support/ticket"><i class="fa fa-tag"></i> Support </a>
                     </li>
