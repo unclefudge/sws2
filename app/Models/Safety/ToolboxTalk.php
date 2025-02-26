19cda370bd65d4e06b9ab9ef43ccebc07cf9a6d2
Fudge Jordan <fudge@jordan.net.au>
2025-02-26 12:47:41 +1100

    update software

--- app/Models/Safety/ToolboxTalk.php
+++ app/Models/Safety/ToolboxTalk.php
@@ -444,6 +444,7 @@ class ToolboxTalk extends Model
         $email_to[] = (\App::environment('dev', 'prod')) ? $this->owned_by->notificationsUsersEmailType('doc.whs.approval') : env('EMAIL_ME');
         $email_user = (Auth::check() && validEmail(Auth::user()->email)) ? Auth::user()->email : '';
 
+        dd($email_to);
         if ($email_to && $email_user)
             Mail::to($email_to)->cc([$email_user])->send(new \App\Mail\Safety\ToolboxTalkActiveTemplate($this));
         elseif ($email_to)
