9c20ad6f13193434a344405bf1facdf5fe349519
Fudge Jordan <fudge@jordan.net.au>
2025-12-31 11:07:30 +1100

    update software

--- app/Http/Controllers/Misc/ReportController.php
+++ app/Http/Controllers/Misc/ReportController.php
@@ -58,8 +58,7 @@ class ReportController extends Controller
         $fullPath = trim($report->path, '/') . '/' . $report->name;
 
         abort_unless($disk->exists($fullPath), 404);
-
-        dd('here2');
+        
         $filename = $report->name;
         $extension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 
