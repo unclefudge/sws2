6f80ef9cf1873a75146941275e9bfe5e96e318be
Fudge Jordan <fudge@jordan.net.au>
2026-06-30 10:33:24 +1000

    update software

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        'wp/request-designer',
+        'wp/request-designer/step-one',
     ];
 }
