24477aaa9bd5255de6c9ad374940836eb853a4ab
Fudge Jordan <fudge@jordan.net.au>
2026-06-30 10:19:04 +1000

    update software

--- resources/views/misc/request-designer.blade.php
+++ resources/views/misc/request-designer.blade.php
@@ -50,7 +50,7 @@
             min-height: 100vh;
             padding: 52px 40px 38px;
             background: linear-gradient(rgba(255, 255, 255, .86), rgba(255, 255, 255, .86)),
-            /*url('https://images/designer-visit-bg.jpg') center center / cover no-repeat;*/
+            url('https://safeworksite.com.au/images/designer-visit-bg.jpg') center center / cover no-repeat;
         }
 
         /*
