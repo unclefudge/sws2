e1bcf05bfc8ecbf0fb37807ba961daa03daa14a1
Fudge Jordan <fudge@jordan.net.au>
2026-07-07 09:00:37 +1000

    update software

--- resources/views/misc/wp-form/fixed-price-form.blade.php
+++ resources/views/misc/wp-form/fixed-price-form.blade.php
@@ -25,7 +25,7 @@
                             <div class="col-md-12">
                                 <iframe
                                         id="requestFixedPriceStaffFrame"
-                                        src="/wp/staff/request-fixed-price-quotation"
+                                        src="/wp/staff/request-fixed-price"
                                         title="Staff Request a Fixed Price Quotation"
                                         style="width: 100%; border: 0; height: 900px; min-height: 500px; overflow: hidden;"
                                         scrolling="no">
