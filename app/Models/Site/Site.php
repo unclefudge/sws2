5220721d87b827bcd1f3069a38b3a4f8b9b8e55a
Fudge Jordan <fudge@jordan.net.au>
2026-08-13 14:48:02 +1000

    update software

--- app/Models/Site/Site.php
+++ app/Models/Site/Site.php
@@ -39,7 +39,7 @@ class Site extends Model
     public const ZOHO_SYNC_BOOLEAN_FIELDS = ['engineering',];
 
     // Zoho supplies these fields, but SafeWorksite remains authoritative.
-    public const ZOHO_SYNC_EXCLUDED_UPDATE_FIELDS = ['completion_signed',];
+    public const ZOHO_SYNC_EXCLUDED_UPDATE_FIELDS = [];
 
     protected $fillable = [
         // Zoho-owned fields
@@ -53,6 +53,7 @@ class Site extends Model
         'engineering_cert', 'extension_notes', 'completed', 'jobstart_estimate',
         'cc', 'cc_stage', 'fc_plans', 'fc_plans_stage', 'fc_struct', 'fc_struct_stage', 'cf_est', 'cf_est_stage', 'cf_adm', 'cf_adm_stage',
         'eworks', 'pworks', 'aftercare', 'steel', 'status', 'company_id', 'created_by', 'updated_by'];
+
     protected $casts = ['completed' => 'datetime', 'jobstart_estimate' => 'datetime', 'contract_sent' => 'datetime', 'contract_signed' => 'datetime', 'deposit_paid' => 'datetime', 'council_approval' => 'datetime',
         'completion_signed' => 'datetime', 'engineering_cert' => 'datetime', 'construction_rcvd' => 'datetime', 'hbcf_start' => 'datetime', 'forecast_completion' => 'datetime', 'client_occupation' => 'datetime',
         'cp_sent_client' => 'datetime', 'oc_rcvd_date' => 'datetime',];
